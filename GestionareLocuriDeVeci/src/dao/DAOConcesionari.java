8ef0cdf66ba7054490e4e880e43d6cef44c77ae3
anton <antoniu.ienciu@outlook.com>
2015-01-10 16:18:29 +0200

    Resolved Conflicts

--- GestionareLocuriDeVeci/src/dao/DAOConcesionari.java
+++ GestionareLocuriDeVeci/src/dao/DAOConcesionari.java
@@ -3,7 +3,9 @@ package dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.sql.ResultSet;
 
 import domain.Concesionar;
 
@@ -13,35 +15,36 @@ public class DAOConcesionari implements IDAOConcesionari {
 	PreparedStatement PSInsert = null;
 	PreparedStatement PSSelect = null;
 	PreparedStatement PSDelete = null;
-    
-    public DAOConcesionari() {
-    	
-    	connection = ConnectionFactory.getConnection();
-    }
-    
-    public void insert(Concesionar concesionar) throws SQLException{
-    	
-    	try{
-    	 String insertTable = "INSERT INTO concesionari" + "(domiciliu, nrChitanta, cnpConcesionar, idLocDeVeci) VALUES" + "(? , ?, ?, ?)";
-    	 PSInsert = connection.prepareStatement(insertTable);
-    	 PSInsert.setString(1, concesionar.getDomiciliu());
-    	 PSInsert.setInt(2, concesionar.getNrChitanta());
-    	 PSInsert.setString(3, concesionar.getCnpConcesionar());
-    	 PSInsert.setInt(4, concesionar.getIdLocDeVeci());
-    	 PSInsert.executeUpdate();
-    	}catch(SQLException ex){
-    		throw new SQLException("Error when trying to insert the: " + concesionar + ":" + ex.getMessage());
-    	}finally{
-    		if(PSInsert !=null){
-                PSInsert.close();
-            }
-    	}
-    }
+	PreparedStatement PSUpdate = null;
+
+	public DAOConcesionari() {
+
+		connection = ConnectionFactory.getConnection();
+	}
+
+	public void insert(Concesionar concesionar) throws SQLException{
+
+		try{
+			String insertTable = "INSERT INTO concesionari" + "(domiciliu, nrChitanta, cnpConcesionar, idLocDeVeci, deleted) VALUES" + "(? , ?, ?, ?, false)";
+			PSInsert = connection.prepareStatement(insertTable);
+			PSInsert.setString(1, concesionar.getDomiciliu());
+			PSInsert.setInt(2, concesionar.getNrChitanta());
+			PSInsert.setString(3, concesionar.getCnpConcesionar());
+			PSInsert.setInt(4, concesionar.getIdLocDeVeci());
+			PSInsert.executeUpdate();
+		}catch(SQLException ex){
+			throw new SQLException("Error when trying to insert the: " + concesionar + ":" + ex.getMessage());
+		}finally{
+			if(PSInsert !=null){
+				PSInsert.close();
+			}
+		}
+	}
 
 	@Override
 	public void update(Concesionar concesionar) throws SQLException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -63,10 +66,30 @@ public class DAOConcesionari implements IDAOConcesionari {
 
 	@Override
 	public List<Concesionar> getAll() throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Concesionar> concesionari = new ArrayList<Concesionar>();
+		try
+		{
+			String selectStatement = "select * from Concesionari where deleted=false";
+			PSSelect = connection.prepareStatement(selectStatement);
+			ResultSet rs = PSSelect.executeQuery();
+			Concesionar c;
+			
+			while(rs.next())
+			{
+				c = new Concesionar(rs.getInt(1), rs.getString(2), rs.getInt(3), rs.getString(4), rs.getInt(5));
+				concesionari.add(c);
+			}
+		}
+		catch(SQLException ex)
+		{
+			throw new SQLException("Error in getAll Concesionari: " + ex.getMessage());
+		}
+		finally{
+			if(PSSelect!=null)
+				PSSelect.close();
+		}
+		return concesionari;
 	}
-
 	@Override
 	public Concesionar getConcesionarById(int idConcesionar) {
 		// TODO Auto-generated method stub
