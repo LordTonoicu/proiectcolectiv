751d8a9a1c26f879b76fd60ecadeda77ddd6e1a8
Ioana <Ioana@Ioana>
2015-01-16 21:30:41 +0200

    aaaaaa

--- GestionareLocuriDeVeciWeb/WebContent/jsp/js/validations/addContractValidator.js
+++ GestionareLocuriDeVeciWeb/WebContent/jsp/js/validations/addContractValidator.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$( "#updateContract" ).click(function() {
+	$( "#adaugaContract" ).click(function() {
 		if ($("#nrContract").val() == "" || $("#dataEliberare").val()==""|| $("#cnpConcesionar1").val()==""|| $("#cnpConcesionar2").val()==""){
 			event.preventDefault();
 			alert("Va rugam sa introduceti date valide!");
